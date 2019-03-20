122ebfbeba49d34a803a16bb44f8adf279c312d6
Dipika <dipika.rauniyar@gmail.com>
2019-03-20 15:52:37 -0700

    devtool working for automatic reloading

--- src/main/java/com/test/Rest/DemoController.java
+++ src/main/java/com/test/Rest/DemoController.java
@@ -1,5 +1,6 @@
 package com.test.Rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,26 @@ import java.time.LocalDateTime;
 @RestController
 public class DemoController {
 
+    @Value("${application.greeting}")
+    private String greeting;
+
     @GetMapping("/")
     public  String start() {
-        return "Hello!!! The time on server is "+ LocalDateTime.now();
+        return greeting + "!!! The time on server is "+ LocalDateTime.now();
     }
 
+    @GetMapping("/EveningGreeting")
+    public  String eveningGreeting() {
+        return greeting + " Good Evening!!! The time on server is "+ LocalDateTime.now();
+    }
+
+    @GetMapping("/morningGreeting")
+    public  String morningGreeting() {
+        return greeting + " Good Morning!!! The time on server is "+ LocalDateTime.now();
+    }
+
+    @GetMapping("/greet")
+    public  String afternoonGreeting() {
+        return greeting + " Good Afternoon!!! The time on server is "+ LocalDateTime.now();
+    }
 }
